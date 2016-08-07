f28efbec1739a8c44181a33eeca2c2af0224840b
Gem Gutierrez <gutierrezgema44@rocketmail.com>
2016-08-07 06:29:15 -0700

    Babylyn 08-07

---
+++ selectfacility.php
@@ -0,0 +1,66 @@
+<?php include("head.php");?>
+<!-- === BEGIN CONTENT === -->
+<div class="body team">
+				<div>
+					<span>SELECT FACILITY AND EQUIPMENT</span>
+					
+				</div>
+				<div class="sidebar">
+					<div class="news">
+						<span>Latest News</span>
+						<ul>
+							<li>
+								<a href="news-single.php">ADDITIONAL CCTV CAM</a> <span>Posted on 17 June 2016</span>
+							</li>
+							<li>
+								<a href="tempnews-single2.php">INSTALLATION OF...</a> <span>Posted on 16 June 2016</span>
+							</li>
+							<li>
+								<a href="tempnews-single3.php">INAUGURATION OF...</a> <span>Posted on 19 September 2015</span>
+							</li>
+							<li>
+								<a href="tempnews-single4.php">DISTRIBUTION OF...</a> <span>Posted on 20 March 2015</span>
+							</li>
+							
+							
+						</ul> <br>
+						<a href="news.php">Read More</a>
+					</div>
+					<div class="section">
+						<span>Services</span>
+						<ul>
+							<li>
+								<a href="services.php">FACILITY RESERVATION</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<span>Contact Us</span>
+						<ul>
+							<li>
+								<p>
+									FACEBOOK PAGE: <a href="https://www.facebook.com/Barangay-San-Isidro-Galas-Quezon-City-504862119632919">https://www.facebook.com/Barangay-San-Isidro-Galas-Quezon-City-504862119632919</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									WEBSITE: <a href="http://www.website.com/">www.SanIsidroGalas.hostinger.com</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									EMAIL: <a href="mailto:brgy.sanisidrogalasqc@gmail.com">brgy.sanisidrogalasqc@gmail.com</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									CONTACT NUMBER: 415-4212 / 415-4601
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+	
+<!-- === END CONTENT === -->
+<?php include("footer1.php");?>
---
+++ tempnews-single2.php
@@ -0,0 +1,74 @@
+<?php include("head.php");?>
+			<!-- === BEGIN CONTENT === -->
+						<div class="body news-single">
+				<div>
+					<span>News</span> <a href="news.php" id="paging">< Back</a>
+					<div>
+						<img src="images/news/sample8.jpg" alt="">
+						<h3>INSTALLATION OF PUBLIC ADDRESS SYSTEM</h3>
+						<span>16th June 2016 | Posted by <span>...</span></span>
+						<p>
+							A priority project of BRGY. SAN ISIDRO GALAS COUNCIL 2016 headed by: P/B John M. Reyno & Council, conducts installation of Public Address (PA) System to different street  to improve security, safety anf for fast and effective dissemination.
+						</p> <br>
+						<a href="news.php" id="paging">< Back</a>
+					</div>
+					
+				</div>
+				<div class="sidebar">
+					<div class="news">
+						<span>Latest News</span>
+						<ul>
+							<li>
+								<a href="news-single.php">ADDITIONAL CCTV CAM</a> <span>Posted on 17 June 2016</span>
+							</li>
+							<li>
+								<a href="tempnews-single2.php">INSTALLATION OF...</a> <span>Posted on 16 June 2016</span>
+							</li>
+							<li>
+								<a href="tempnews-single3.php">INAUGURATION OF...</a> <span>Posted on 19 September 2015</span>
+							</li>
+							<li>
+								<a href="tempnews-single4.php">DISTRIBUTION OF...</a> <span>Posted on 20 March 2015</span>
+							</li>
+							
+						</ul> <br>
+						<a href="news.php">Read More</a>
+					</div>
+					<div class="section">
+						<span>Services</span>
+						<ul>
+							<li>
+								<a href="services.php">FACILITY RESERVATION</a>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<span>Contact Us</span>
+						<ul>
+							<li>
+								<p>
+									FACEBOOK PAGE: <a href="https://www.facebook.com/Barangay-San-Isidro-Galas-Quezon-City-504862119632919">https://www.facebook.com/Barangay-San-Isidro-Galas-Quezon-City-504862119632919</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									WEBSITE: <a href="http://www.website.com/">www.SanIsidroGalas.hostinger.com</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									EMAIL: <a href="mailto:brgy.sanisidrogalasqc@gmail.com">brgy.sanisidrogalasqc@gmail.com</a>
+								</p>
+							</li>
+							<li>
+								<p>
+									CONTACT NUMBER: 415-4212 / 415-4601
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			
+<!-- === END CONTENT === -->
+<?php include("footer1.php");?>
