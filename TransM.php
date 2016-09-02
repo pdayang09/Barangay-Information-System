e76e95bf300cdb01b55fe84b0fc7d847faaef9f7
JuliusRei <morcillajr0912@gmail.com>
2016-09-02 17:58:30 +0800

    Add files via upload

--- TransM.php
+++ TransM.php
@@ -3,11 +3,57 @@
 $sid = $_POST['sid'];?>
 	
 <form method = POST>
+<?php
+require('connection.php');
+
+$id = $_POST['sid'];
+$query = "Select concat(strLastName,', ',strFirstName) as Name, TIMESTAMPDIFF(YEAR,dtBirthdate,curdate()) as age, charGender,intForeignHouseholdNo from tblHousemember where intMemberNo = '$id'";
+$sql = mysqli_query($con,$query);
+$row = mysqli_fetch_object($sql);
+
+$name = $row->Name;
+$bday = $row->age;
+$g = $row->charGender;
+$houseno = $row->intForeignHouseholdNo;
+if($g == 'M'){
+	$Gender = 'Male';
+}
+else{$Gender = 'Female';}
+
+?>
+<div class="form-group" id = "contact-div">				<p><font face = "cambria" size = 4 color = "grey"> Full Name: </font></p>
+	<div class="col-sm-9">
+
+		<input id="DelName" class="form-control input-group-lg reg_name" type= text value = '<?php echo $name?>'  readonly> 
+		
+	</div>
+	
+	
+</div>
+<br><br><br>
+<div class="form-group" id = "contact-div">				<p><font face = "cambria" size = 4 color = "grey"> Age: </font></p>
+	<div class="col-sm-9">
+
+		<input id="DelBday" class="form-control input-group-lg reg_name" type= text value = <?php echo $bday?>  readonly> 
+		
+	</div>
+	
+	
+</div>
+<br><br><br>
+<div class="form-group" id = "contact-div">				<p><font face = "cambria" size = 4 color = "grey"> Gender: </font></p>
+	<div class="col-sm-9">
+
+		<input id="DelGend" class="form-control input-group-lg reg_name" type= text value = <?php echo $Gender?>  readonly> 
+		
+	</div>
+	<br><br><br>
+	<center>
+	<p>Transfer to a:</p>
       <button = submit name = "Exist"  <?php echo "value = '".$sid."'";?> class="btn btn-default">Existing Household</button>
 							   <button type = submit name = 'NewH' class="btn btn-default" <?php
 							   echo "value = '".$sid."'";
 							   
-							   require('connection.php');
 				
 				 $sql = "Select dtBirthdate From tblhousemember where intMemberNo = '$sid'";
 							   $query = mysqli_query($con,$sql);
@@ -16,5 +62,5 @@ $sid = $_POST['sid'];?>
 							   if((date("Y-m-d") - $bday)<18){
 								   echo "disabled";
 							   }
-							   else{}?>>New Household</button>		  
+							   else{}?>>New Household</button>	</center>	  
 							 </form>
