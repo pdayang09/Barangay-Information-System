5e858a1e4f354f5ab2b84ad3d6706e51841447a8
JuliusRei <morcillajr0912@gmail.com>
2016-09-11 01:11:48 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,15 +2,7 @@
 	if($con= mysqli_connect("localhost","root","","brgydb")){
 		
 	}else{
-		die("Unable to connect to database1".mysql_error());
+		die("unable to connect because".mysql_error());
 	}
 	mysqli_select_db($con, "brgydb");
-?>
-<?php
-	$cn = mysql_connect('localhost','root','') or die ('Can not');
-
-	if ($cn)
-	{
-		mysql_select_db('brgydb',$cn) or die('Can not connect with database!');
-	}
 ?>
